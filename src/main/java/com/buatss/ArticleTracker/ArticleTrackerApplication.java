fafdc8d041e893fd9c65d2a6ad2b2e6cfca859f2
Tomasz Bułat <buatss@gmail.com>
2023-04-03 13:16:54 +0200

    initial commit

---
+++ src/main/java/com/buatss/ArticleTracker/ArticleTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.buatss.ArticleTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buatss/ArticleTracker/ArticleTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buatss.ArticleTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
