d99ae56eb08a5cabf598ab6a84c0ad1ab9ea5e52
Tomasz Bułat <buatss@gmail.com>
2023-04-17 13:54:37 +0200

    stop printing most logs to console

--- src/main/java/com/buatss/ArticleTracker/config/WebDriverConfig.java
+++ src/main/java/com/buatss/ArticleTracker/config/WebDriverConfig.java
@@ -2,15 +2,21 @@ package com.buatss.ArticleTracker.config;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebDriverConfig {
-
     @Bean
     public WebDriver webDriver() {
         System.setProperty("webdriver.gecko.driver", System.getenv("geckodriver"));
-        return new FirefoxDriver();
+
+        FirefoxOptions options = new FirefoxOptions();
+
+        options.addPreference("devtools.console.stdout.content", false);
+        options.addPreference("devtools.console.stderr.content", false);
+
+        return new FirefoxDriver(options);
     }
 }
