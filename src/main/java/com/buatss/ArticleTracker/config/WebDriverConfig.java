99e6fcb4b0155a2f1e2075579624df3528e71a70
Tomasz Bułat <buatss@gmail.com>
2023-09-23 12:41:10 +0200

    AR-9-report-statistics-on-e-mail add reporting failure of build with attached logs add build on branch-create/update

--- src/main/java/com/buatss/ArticleTracker/config/WebDriverConfig.java
+++ src/main/java/com/buatss/ArticleTracker/config/WebDriverConfig.java
@@ -6,13 +6,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Slf4j
 public class WebDriverConfig {
     @Bean
-    @Profile(value ={"dev","prod"})
     public WebDriver webDriver() {
         System.setProperty("webdriver.gecko.driver", System.getenv("geckodriver"));
 
@@ -25,13 +23,6 @@ public class WebDriverConfig {
         options.addPreference("devtools.console.stdout.content", false);
         options.addPreference("devtools.console.stderr.content", false);
 
-        return new FirefoxDriver(options);
-    }
-
-    @Bean
-    @Profile("default")
-    public WebDriver webDriverNoGecko() {
-        FirefoxOptions options = new FirefoxOptions();
         return new FirefoxDriver(options);
     }
 }
