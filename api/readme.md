6c66287132c42b7bd1e3d971b216d1c53bc521fa
Tomasz Bułat <buatss@gmail.com>
2023-11-12 20:43:45 +0100

    add CORS middleware and endpoint for latest articles

--- api/readme.md
+++ api/readme.md
@@ -6,4 +6,5 @@
 
 - `/` - default with welcome message  
 - `/article` - accepts only article id e.g. `/article?id=1,2,3`
+- `/article/latest` - accepts only limit e.g. `/article/latest?limit=10`, limit cannot be greater than 100.
 - `/media_site` - accepts only article id e.g. `/media_site?id=1,2,3`
--- api/routes/article.js
+++ api/routes/article.js
@@ -21,7 +21,7 @@ connection.connect((err) => {
 router.get('/', (req, res) => {
   const articleId = req.query.id;
   if (!articleId) {
-    res.status(400).send('No "id" parameter in query.');
+    res.status(400).send('No "id" parameter in query. Valid parameter is e.g. "id=5" or like "id=1,2,3"');
     return;
   }
 
@@ -36,6 +36,32 @@ router.get('/', (req, res) => {
 
     if (results.length === 0) {
       res.send('No article with such id');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+router.get('/latest', (req, res) => {
+  const limit = req.query.limit;
+  const realLimit = Math.min(parseInt(req.query.limit, 10) || 20, 100);
+
+  if (!limit) {
+    res.status(400).send('No limit param, valid is e.g. "limit=10", it cannot be creater than 100.');
+    return;
+  }
+
+  const query = `SELECT * FROM article ORDER BY upload_date DESC LIMIT ${realLimit}`;
+
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Query error: ', err);
+      res.status(500).send('Query error');
+      return;
+    }
+
+    if (results.length === 0) {
+      res.send('No articles with such id');
     } else {
       res.json(results);
     }
