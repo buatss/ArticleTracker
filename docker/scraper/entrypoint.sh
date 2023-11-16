5b8cc0bd0270c9cad4bf7cba8954977f4b994c7c
Tomasz Bułat <buatss@gmail.com>
2023-11-16 12:36:01 +0100

    fix at image duplication, at container waits for db to launch before starting scrapper

--- docker/scraper/entrypoint.sh
+++ docker/scraper/entrypoint.sh
@@ -5,4 +5,5 @@ mv geckodriver /usr/local/bin/
 chmod +rwx /usr/local/bin/*
 export geckodriver=/usr/local/bin/geckodriver
 
+./wait-for-it.sh -h mysql_db -p 3306 -t 60
 java -jar -Dspring.profiles.active=prod /at.jar
